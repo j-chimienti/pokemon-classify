752e31dbc876237bb2d302cde5f58a3e60c321d7
joe <jchimien@gmail.com>
2018-11-07 16:45:10 -0500

    Updates

---
+++ precache-manifest.582de4c6388bf5d927a3a02135166652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4f594f6de13312031379f1a9c575131",
+    "url": "/pokemon-classify/static/media/pokemon.a4f594f6.jpg"
+  },
+  {
+    "revision": "ed55a3b57d68ad72cc64",
+    "url": "/pokemon-classify/static/js/runtime~main.ed55a3b5.js"
+  },
+  {
+    "revision": "f53627219527a8a66482",
+    "url": "/pokemon-classify/static/js/main.f5362721.chunk.js"
+  },
+  {
+    "revision": "5bf85bcf151db5433a16",
+    "url": "/pokemon-classify/static/js/1.5bf85bcf.chunk.js"
+  },
+  {
+    "revision": "f53627219527a8a66482",
+    "url": "/pokemon-classify/static/css/main.a50d00e3.chunk.css"
+  },
+  {
+    "revision": "5bf85bcf151db5433a16",
+    "url": "/pokemon-classify/static/css/1.67174783.chunk.css"
+  },
+  {
+    "revision": "71d607166498bf8a567cb3116bb00ccb",
+    "url": "/pokemon-classify/index.html"
+  }
+];
